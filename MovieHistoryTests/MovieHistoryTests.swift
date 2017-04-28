0432c321e7bd5d4fd89d00320107c480da727eca
stash4 <high.mackey@gmail.com>
2017-04-28 14:24:41 +0900

    Initial Commit

---
+++ MovieHistoryTests/MovieHistoryTests.swift
@@ -0,0 +1,36 @@
+//
+//  MovieHistoryTests.swift
+//  MovieHistoryTests
+//
+//  Created by Hironobu Makado on 2017/04/28.
+//  Copyright © 2017年 stash4. All rights reserved.
+//
+
+import XCTest
+@testable import MovieHistory
+
+class MovieHistoryTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
